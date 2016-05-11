0177ec0cb03c3893685306e3863617e7497bebaf
Mallory <Mallory@Mallory-HP>
2016-05-11 09:38:27 -0600

    05-11-16 deleted extra function

--- hogwarts/src/hogwarts/Hogwarts.java
+++ hogwarts/src/hogwarts/Hogwarts.java
@@ -5,47 +5,21 @@
  */
 package hogwarts;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+
 
 /**
  *
  * @author Mallory
  */
-public class Hogwarts extends Application {
+public class Hogwarts  {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+      
     }
     
 }
