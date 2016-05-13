ba687784192afb96d3218ec9b73d961647a829d1
rachelbarnes <rachelbarnes@10.18.148.113>
2016-05-13 09:33:04 -0600

    5-13-2016 Finished Map class

--- hogwarts/src/byui/CIT260/Hogwarts/model/Map.java
+++ hogwarts/src/byui/CIT260/Hogwarts/model/Map.java
@@ -6,6 +6,7 @@
 package byui.CIT260.Hogwarts.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -13,9 +14,80 @@ import java.io.Serializable;
  */
 public class Map implements Serializable {
     //class instance variables
-    private String noRows;
-    private String noColumns;
+    private double numberOfRows;
+    private double numberOfColumns;
     private String name;
+
+    public Map() {
+    }
+    
+    public double getNumberOfRows() {
+        return numberOfRows;
+    }
+
+    /**
+     *
+     * @param numberOfRows
+     */
+    public void setNumberOfRows(double numberOfRows) {
+        this.numberOfRows = numberOfRows;
+    }
+
+    public double getNumberOfColumns() {
+        return numberOfColumns;
+    }
+
+    public void setNumberOfColumns(double numberOfColumns) {
+        this.numberOfColumns = numberOfColumns;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.numberOfRows);
+        hash = 53 * hash + Objects.hashCode(this.numberOfColumns);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "numberOfRows=" + numberOfRows + ", numberOfColumns=" + numberOfColumns + ", name=" + name + '}';
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (!Objects.equals(this.numberOfRows, other.numberOfRows)) {
+            return false;
+        }
+        if (!Objects.equals(this.numberOfColumns, other.numberOfColumns)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
     
     
 }
