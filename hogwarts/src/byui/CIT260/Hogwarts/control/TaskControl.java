424649d8632bc955697f0dbe603d486d0a9ffe13
Mallory <Mallory@Mallory-HP>
2016-06-09 09:27:42 -0600

    Created the third task view 06-08-2016

--- hogwarts/src/byui/CIT260/Hogwarts/control/TaskControl.java
+++ hogwarts/src/byui/CIT260/Hogwarts/control/TaskControl.java
@@ -13,14 +13,6 @@ public class TaskControl {
 
     public static double calcAreaOfTriangle(double base, double height) {
 
-        if (height <= 0) {
-            return -1;
-
-        }
-
-        if (base <= 0 || base > 36) {
-            return -1;
-        }
         double area = (base * height) / 2;
 
         return area;
@@ -30,10 +22,6 @@ public class TaskControl {
         //calculate area of triangle
         double area = TaskControl.calcAreaOfTriangle(base, height);
                 
-        //if failed, return error code
-        if (area < 0) {
-            return false;
-        }
         
         //if successful, check solution equals area 
         if (area == solution) {
@@ -44,27 +32,15 @@ public class TaskControl {
     
     public static double calcAreaOfCylinder(double radius, double height) {
 
-        if (radius <= 0) {
-        return -1;
-        }
-        if (height <= 0 || height > 28){
-        return -1;
-        }
-        
-        double area = (Math.round((2 * Math.PI * radius * height) + (2 * Math.PI * Math.pow(radius,2)))); 
+      double area = (Math.round((2 * Math.PI * radius * height) + (2 * Math.PI * Math.pow(radius,2)))); 
       
-        return area;
+      return area;
 
     }
     public static boolean checkCylinderArea(double radius, double height, double solution) {
         //calculate area of Cylinder
         double area = TaskControl.calcAreaOfCylinder(radius, height);
                 
-        //if failed, return error code
-        if (area < 0) {
-            return false;
-        }
-    
         if (area == solution) {
             return true;
         }
